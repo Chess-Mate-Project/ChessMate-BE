f32927577ea88ef68fd149eacc7c303b0bdb0525
nuza <wjdansrud092208@gmail.com>
2026-02-08 17:08:45 +0900

    Add www.chessladder.org to CORS allowed origins

--- chessmate-api/src/main/java/com/chessmate/api/config/CorsConfig.java
+++ chessmate-api/src/main/java/com/chessmate/api/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:5173" , "http://localhost:5174", "https://chessladder.org")
+        .allowedOrigins("http://localhost:5173", "http://localhost:5174", "https://chessladder.org", "https://www.chessladder.org")
         .allowedMethods(ALLOWED_METHODS)
         .allowedHeaders("*")
         .allowCredentials(true)
